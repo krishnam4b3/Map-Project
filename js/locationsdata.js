ed42b96fa69278a7399d188bb0fafe657ab6d54b
krishnam4b3 <38848838+krishnam4b3@users.noreply.github.com>
2018-06-14 22:38:46 +0530

    Add files via upload

---
+++ js/locationsdata.js
@@ -0,0 +1,49 @@
+var myLocations = [
+    {
+        title: 'Geetha Multiplex',
+        lat: 16.5466,
+        lng: 81.5173,
+        type: 'Mall',
+    },
+    {
+        title: 'Spice Magic Restaurant',
+        lat: 16.5455,
+        lng: 81.5201,
+        type: 'Restaurant'
+        
+    },
+    {
+        title: 'Bhimavaram Town Railway Station',
+        lat: 16.5475,
+        lng: 81.519,
+        type:  'Railway Station'
+        
+    },
+    {
+        title: 'Someswara Swamy Temple',
+        lat: 16.542575,
+        lng: 81.533123,
+        type:  'Temple'
+        
+    },
+    {
+        title: 'Bhimavaram 1-Town Police Station',
+        lat: 16.5435,
+        lng: 81.5197,
+        type: 'Police Station'
+        
+    },
+    {
+        title: 'Spencers Supermarket',
+        lat: 16.5428728,
+        lng: 81.5117633,
+        type: 'Supermarket'
+        
+    },
+    {
+        title: 'Ananda Inn Hotel',
+        lat: 16.5436,
+        lng: 81.5131,
+        type: 'Hotel'
+    },
+]
---
+++ js/mapview.js
@@ -0,0 +1,91 @@
+var styles = [
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#4ddbff"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "color": "#efefef"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#262626"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#595959"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#cccccc"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#618132"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 33
+            }
+        ]
+    },
+    {
+        "featureType": "road"
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 50
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "stylers": [
+            {
+                "lightness": 20
+            }
+        ]
+    }
+];
